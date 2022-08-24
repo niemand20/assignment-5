9be78372a8fd936c97ce4ffe0c462779fbb79b72
niemand20 <zahid17290@gmail.com>
2022-08-24 22:29:38 +0100

    players expenses done

--- js/script.js
+++ js/script.js
@@ -18,17 +18,40 @@ function displayPlayers(selectedPlayer){
 
 function addToSelectedPlayers(element){
     const playerName = element.parentNode.children[0].innerText;
-    playersArray.push(playerName);
-    // console.log(playerName);
-    // console.log(playersArray);
-    // if (playersArray.length>5){
-    //     alert("you are adding more than five players");
-        
-    // }
-    // else{
-    //     return;
-    // }
-
+    if (playersArray.length<=4){
+        playersArray.push(playerName);
+    }
+    else{
+        alert("you cannot add more than five players")
+    }
     displayPlayers(playersArray);
-
+    
 }
+
+
+
+
+
+document.getElementById("players-cost-calculate-btn").addEventListener("click",function(){
+    
+    //const numbers = parseFloat(playersArray.length);
+    const inputFieldForPlayers = document.getElementById("per-player-cost-input");
+    const playersFieldInput = parseFloat(inputFieldForPlayers.value);
+    //console.log(playersFieldInput);
+    inputFieldForPlayers.value = "";
+
+    let total=0;
+    for (let i=0; i<=playersArray.length-1; i++){
+        total++
+    }
+    console.log(total)
+
+    const playersCost = document.getElementById("player-total-amount");
+    // const previousPlayersCost = parseFloat(playersCost.innerText);
+    
+     const newPlayersCost = (playersFieldInput *total);
+     console.log(parseFloat(newPlayersCost));
+
+    playersCost.innerText = newPlayersCost;
+    console.log(playersCost.innerText);
+});
